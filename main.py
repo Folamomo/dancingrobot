1f925d0fcc8ba98e2029458954a8080bc37574c8
Folamomo <noga.igor@gmail.com>
2022-01-15 22:34:48 +0100

    initial commit

--- main.py
+++ main.py
@@ -2,11 +2,13 @@ import soundcard as sc
 import numpy as np
 import aubio
 import time
+import matplotlib.animation as animation
+import matplotlib.pyplot as plt
 
 # constants
 samplerate = 44100
-win_s = 2048
-hop_s = win_s // 2
+win_s = 512
+hop_s = win_s // 16
 framesize = hop_s
 
 # set up audio input
@@ -28,9 +30,11 @@ print(sc.all_microphones(True))
 loopback = sc.get_microphone("Monitor", True)
 mic = sc.get_microphone("Family", False)
 
+
 with loopback.recorder(samplerate, 1) as recorder:
 
     # main loop
+    prev = 0
     while True:
         try:
             # read data from audio input
@@ -44,13 +48,19 @@ with loopback.recorder(samplerate, 1) as recorder:
             o = onset(samples)
             # print(t)
             # print(tempo.get_bpm())
-            if o:
-                print("beat", end="\t", flush=True)
-            # compute energy of current block
             energy = np.sum(samples**2)/len(samples)
+            # print(energy, prev)
+            # if (energy - prev)/(prev + 0.00000000001) > 1:
+            if (t):
+                # print(tempo.get_bpm())
+                print("beat", end="\t", flush=True)
+            prev = energy
+            # compute energy of current block
             # do something with the results
             # print("{:10.4f} {:10.4f}".format(freq, energy))
-            # print(freq)
+            # print(energy)
         except KeyboardInterrupt:
             print("Ctrl+C pressed, exiting")
             break
+
+
